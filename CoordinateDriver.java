c7d72da5a11ecc216d08aeef70d524ee7eccc289
egeozer <ege.ozer@mail.mcgill.ca>
2017-01-27 11:06:54 -0500

    Lab3Navigation

---
+++ CoordinateDriver.java
@@ -0,0 +1,126 @@
+/*
+ * SquareDriver.java
+ */
+package Navigation;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class CoordinateDriver {
+	private static final int FORWARD_SPEED = 250;
+	private static final int ROTATE_SPEED = 150;
+	EV3LargeRegulatedMotor leftMotor; EV3LargeRegulatedMotor rightMotor;
+	double leftRadius; double rightRadius; double width;
+	
+	
+	Odometer odometer;
+	
+
+	public CoordinateDriver(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
+			double leftRadius, double rightRadius, double width, Odometer odometer){
+		this.leftMotor=leftMotor;
+		this.rightMotor=rightMotor;
+		this.leftRadius=leftRadius;
+		this.rightRadius=rightRadius;
+		this.width=width;
+		this.odometer = odometer;
+		//getx = odometer.getX();
+		
+		for (EV3LargeRegulatedMotor motor : new EV3LargeRegulatedMotor[] { leftMotor, rightMotor }) {
+			motor.stop();
+			motor.setAcceleration(300);
+		}
+	}
+	
+	
+	void travelTo(double x, double y){
+		double currentX = odometer.getX();
+		double currentY = odometer.getY();
+		double tempX = x-currentX;
+		double tempY = y-currentY;
+		double distance;
+		double angle;
+		for(int i = 0; i<10; i++){
+			System.out.println("------------------------------");
+				System.out.println(odometer.getX());
+				System.out.println(odometer.getY());
+				System.out.println("------------------------------");
+		}
+		
+		//double xPos = odometer.getY();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e){}
+		
+		distance = Math.sqrt(  Math.pow((tempX), 2)    + Math.pow((tempY), 2)   );
+		
+		if(tempX>0 && tempY>= -2 && tempY<=2)
+			angle = 90;
+		else if(tempX<0 && tempY >= -2 && tempY<=2)
+			angle = -90;
+		
+		else if(tempY >0 && tempX >= -2 && tempX<=2)
+			angle = 0;
+		else if(tempY<0 && tempX >= -2 && tempX<=2){
+			angle = 180;
+			
+		}
+		else if (tempY<-2 && tempX<-2){
+		angle = 90-Math.atan((tempY)/(tempX))*180/Math.PI-180; 
+		}
+		else{
+		angle = 90-Math.atan((tempY)/(tempX))*180/Math.PI; 
+		System.out.println("ELSLELELELELELELELEL");
+		}//test 1 1
+		
+		
+		leftMotor.setSpeed(ROTATE_SPEED);
+		rightMotor.setSpeed(ROTATE_SPEED);
+				
+		leftMotor.rotate(convertAngle(leftRadius, width, angle), true);
+		rightMotor.rotate(-convertAngle(rightRadius, width, angle), false);
+		
+		leftMotor.setSpeed(FORWARD_SPEED);
+		rightMotor.setSpeed(FORWARD_SPEED);
+		
+		leftMotor.rotate(convertDistance(leftRadius, distance), true);
+		rightMotor.rotate(convertDistance(rightRadius, distance), false);
+		
+		for(int i = 0; i<10; i++){
+			System.out.println("------------------------------");
+				System.out.println(odometer.getX());
+				System.out.println(odometer.getY());
+				System.out.println("------------------------------");
+		}
+		leftMotor.setSpeed(ROTATE_SPEED);
+		rightMotor.setSpeed(ROTATE_SPEED);
+		
+		leftMotor.rotate(convertAngle(leftRadius, width, -angle), true);
+		rightMotor.rotate(-convertAngle(rightRadius, width, -angle), false);
+		
+		
+
+		//odometer.setX(odometer.getX());
+		//odometer.setY(odometer.getY());
+
+		
+		
+	}
+	
+	void turnTo(double x, double y){
+		
+	}
+	
+	boolean isNavigating(){
+		if(leftMotor.isMoving())
+			return true;
+		return false;
+	}
+
+	private static int convertDistance(double radius, double distance) {
+		return (int) ((180.0 * distance) / (Math.PI * radius));
+	}
+
+	private static int convertAngle(double radius, double width, double angle) {
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}
+}
---
+++ lab3.java
@@ -0,0 +1,83 @@
+
+package Navigation;
+
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class lab3 {
+	// Static Resources:
+	// Left motor connected to output A
+	// Right motor connected to output D
+	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+
+	// Constants
+	public static final double WHEEL_RADIUS = 2.14;
+	public static final double TRACK = 14.5;  
+
+	public static void main(String[] args) {
+		int buttonChoice;
+		Odometer odometer = new Odometer(leftMotor, rightMotor);
+		final CoordinateDriver driver = new CoordinateDriver(leftMotor, rightMotor, WHEEL_RADIUS, WHEEL_RADIUS, TRACK, odometer);
+
+		// some objects that need to be instantiated
+		
+		final TextLCD t = LocalEV3.get().getTextLCD();
+		//Odometer odometer = new Odometer(leftMotor, rightMotor);
+		OdometryDisplay odometryDisplay = new OdometryDisplay(odometer,t);
+		//OdometryCorrection odometryCorrection = new OdometryCorrection(odometer);
+
+		do {
+			// clear the display
+			t.clear();
+
+			// ask the user whether the motors should drive in a square or float
+			t.drawString("< Left | Right >", 0, 0);
+			t.drawString("       |        ", 0, 1);
+			t.drawString(" Float | Drive  ", 0, 2);
+			t.drawString("motors | to a   ", 0, 3);
+			t.drawString("       | Coord ", 0, 4);
+
+			buttonChoice = Button.waitForAnyPress();
+		} while (buttonChoice != Button.ID_LEFT
+				&& buttonChoice != Button.ID_RIGHT);
+
+		if (buttonChoice == Button.ID_LEFT) {
+			
+			leftMotor.forward();
+			leftMotor.flt();
+			rightMotor.forward();
+			rightMotor.flt();
+			
+			odometer.start();
+			odometryDisplay.start();
+		//	odometryCorrection.start();
+			
+		} else {
+			// start the odometer, the odometry display and (possibly) the
+			// odometry correction
+			
+			odometer.start();
+			odometryDisplay.start();
+			//odometryCorrection.start();
+
+			// spawn a new Thread to avoid SquareDriver.drive() from blocking
+			(new Thread() {
+				public void run() {
+					driver.travelTo(60,30);
+					driver.travelTo(30,30);
+				//	driver.travelTo(60,30);
+				//	driver.travelTo(90,30);
+					//driver.travelTo(30,-60);
+					//driver.travelTo(0,60);//CoordinateDriver.drive(leftMotor, rightMotor, WHEEL_RADIUS, WHEEL_RADIUS, TRACK);
+					//CoordinateDriver.travelTo(3,3);
+				}
+			}).start();
+		}
+		
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		System.exit(0);
+	}
+}
